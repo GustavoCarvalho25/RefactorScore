8e96b9cdbc34a2f636ea302effe378e5be2ec0ab
CauetMenezes <cauetcorrea@outlook.com>
2025-10-29 20:23:05 -0300

    Feat: @Cauet-Menezes - Commit das interfaces

--- Front/refactor-score-frontend/src/interfaces/CommitAnalysis.ts
+++ Front/refactor-score-frontend/src/interfaces/CommitAnalysis.ts
@@ -4,16 +4,20 @@ import { Suggestion } from './Suggestion';
 
 export interface CommitAnalysis {
   id: string;
-  commitId: string;
-  author: string;
-  email: string;
-  commitDate: string;
-  analysisDate: string;
-  language: string;
-  addedLines: number;
-  removedLines: number;
-  files: CommitFile[];
-  suggestions: Suggestion[];
+  commitId?: string;
+  author?: string;
+  email?: string;
+  commitDate?: string;
+  analysisDate?: string;
+  language?: string;
+  addedLines?: number;
+  removedLines?: number;
+  files?: CommitFile[];
+  filesDetails?: CommitFile[];
+  suggestions?: Suggestion[];
   rating?: CleanCodeRating;
-  overallNote: number;
+  OverallNote?: number;
+  commitAverage?: number;
+  data?: any;
+  note?: number;
 }
